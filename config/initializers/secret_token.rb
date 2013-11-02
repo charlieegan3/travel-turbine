2922252df0ba6f8aa055c8182ae8a10660d34573
Charlie <charlieegan3@gmail.com>
2013-11-02 14:54:57 +0000

    Added rails shell

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TravelPlanner::Application.config.secret_key_base = '930c268741a72edbc14a8208891c0cbf58c0e3bfef0ac210bc6c3b43ce6d121202bf6eda80fba8c11c8c67eeca11b6fc561af27c9e715adbf1379ae428c8a773'
