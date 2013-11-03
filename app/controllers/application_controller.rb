f81926b3893182e1e7be74694b63db736e5db77f
Charlie <charlieegan3@gmail.com>
2013-11-03 16:03:42 +0000

    added new methods to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,117 +3,151 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-	#make a request to national rail like: train("INV", "ABD", "211113")
-	def train(origin, destination, date)
-		xml = Nokogiri::HTML(open("http://ojp.nationalrail.co.uk/service/timesandfares/#{origin}/#{destination}/#{date}/0900/dep").read)
-		#get prices
-		price_string = xml.css('.opsingle').inner_html
-		price_string.gsub!(/\s+/, "")
-		prices = price_string.scan(/£\d*.\d*/)
-		
-		#get durations
-		duration_string = xml.css('.dur').inner_html
-		duration_string.gsub!(/\s+/, "")
-		duration_string.gsub!(/<abbrtitle="hours">h<\/abbr>/, ":")
-		duration_string.gsub!(/<abbrtitle="minutes">m<\/abbr>/, ",")
-		duration_string.gsub!(/^.*>/, "")
-		duration_string = duration_string.split(',')
-		durations = Array.new
-		duration_string.each do |d|
-			hours = d[0, d.index(':')]
-			mins = d[d.index(':')+1, d.length]
-			if mins[0,1] == "0"
-				mins = mins[1,1]
-			end
-			durations.push(Integer(hours)*60 + Integer(mins))
-		end
-		results  = Array.new
-			prices.count().times do |count|
-			  	pair = [prices[count], durations[count]]
-				results.push(pair)
-			end
-		return results
-	end
+  require 'open-uri'
+
+#make a request to national rail like: train("INV", "ABD", "211113")
+def train(origin, destination, date)
+	url = "http://ojp.nationalrail.co.uk/service/timesandfares/#{origin}/#{destination}/#{date}/0900/dep"
+	xml = Nokogiri::HTML(open(url).read)
+	#get prices
+	price_string = xml.css('.opsingle').inner_html
+	price_string.gsub!(/\s+/, "")
+	prices = price_string.scan(/£\d*.\d*/)
 	
-	#make a request to megabus like: bus("49", "56", "211113")
-	def bus(origin, destination, date)
-		param_date = date[0,2] + "%2f" + date[2,2] + "%2f20" + date[4,2] #megabus uses odd date formats
-		xml = Nokogiri::HTML(open("https://uk.megabus.com/JourneyResults.aspx?originCode=#{origin}&destinationCode=#{destination}&outboundDepartureDate=#{param_date}&passengerCount=1&transportType=-1").read)
-		#get prices
-		price_string = xml.css('ul.standard>li.five>p').to_s
-		price_string.gsub!(/\s+/, "")
-		price_string.gsub!(/[<p>\/]/, '')
-		prices = price_string.split('£')
-		prices.delete_at(0)
-		#get durations
-		dur_string = xml.css('ul.standard>li.three>p').to_s
-		dur_string.gsub!(/\s+/, "")
-		dur_string.gsub!(/></, ',')
-		dur_string.gsub!(/[<p>\/]/, '')
-		dur_string.gsub!('hrs', ':')
-		dur_string.gsub!('mins', '')
-		duration_string = dur_string.split(',')
-		durations = Array.new
-		duration_string.each do |d|
-			hours = d[0, d.index(':')]
-			mins = d[d.index(':')+1, d.length]
-			if mins[0,1] == "0"
-				mins = mins[1,1]
-			end
-			durations.push(Integer(hours)*60 + Integer(mins))
+	#get durations
+	duration_string = xml.css('.dur').inner_html
+	duration_string.gsub!(/\s+/, "")
+	duration_string.gsub!(/<abbrtitle="hours">h<\/abbr>/, ":")
+	duration_string.gsub!(/<abbrtitle="minutes">m<\/abbr>/, ",")
+	duration_string.gsub!(/^.*>/, "")
+	duration_string = duration_string.split(',')
+	durations = Array.new
+	duration_string.each do |d|
+		hours = d[0, d.index(':')]
+		mins = d[d.index(':')+1, d.length]
+		if mins[0,1] == "0"
+			mins = mins[1,1]
 		end
-		#prepare results
-		results  = Array.new
+		durations.push(Integer(hours)*60 + Integer(mins))
+	end
+	results  = Array.new
 		prices.count().times do |count|
-		  	pair = [prices[count], durations[count]]
+		  	pair = [prices[count][1,prices[count].length], durations[count]] #rather messy way of taking out the £ at the last minute!
 			results.push(pair)
 		end
-		return results
+	return results, url
+end
+
+#make a request to megabus like: bus("49", "56", "211113")
+def bus(origin, destination, date)
+	param_date = date[0,2] + "%2f" + date[2,2] + "%2f20" + date[4,2] #megabus uses odd date formats
+	url = "https://uk.megabus.com/JourneyResults.aspx?originCode=#{origin}&destinationCode=#{destination}&outboundDepartureDate=#{param_date}&passengerCount=1&transportType=-1"
+	xml = Nokogiri::HTML(open(url).read)
+	#get prices
+	price_string = xml.css('ul.standard>li.five>p').to_s
+	price_string.gsub!(/\s+/, "")
+	price_string.gsub!(/[<p>\/]/, '')
+	prices = price_string.split('£')
+	prices.delete_at(0)
+	#get durations
+	dur_string = xml.css('ul.standard>li.three>p').to_s
+	dur_string.gsub!(/\s+/, "")
+	dur_string.gsub!(/></, ',')
+	dur_string.gsub!(/[<p>\/]/, '')
+	dur_string.gsub!('hrs', ':')
+	dur_string.gsub!('mins', '')
+	duration_string = dur_string.split(',')
+	durations = Array.new
+	duration_string.each do |d|
+		hours = d[0, d.index(':')]
+		mins = d[d.index(':')+1, d.length]
+		if mins[0,1] == "0"
+			mins = mins[1,1]
+		end
+		durations.push(Integer(hours)*60 + Integer(mins))
 	end
+	#prepare results
+	results  = Array.new
+	prices.count().times do |count|
+	  	pair = [prices[count], durations[count]]
+		results.push(pair)
+	end
+	return results, url
+end
+
+#make a request to tripsta like: plane("INV", "LON", "211113")
+def plane(origin, destination, date)
+	param_date = date[0,2] + "%2f" + date[2,2] + "%2f20" + date[4,2]
+	url = "http://www.tripsta.co.uk/airline-tickets/results?dep=#{origin}&arr=#{destination}&passengersAdult=1&passengersChild=0&passengersInfant=0&class=&airlineCode=&directFlightsOnly=0&extendedDates=0&isRoundtrip=0&obDate=#{param_date}&obTime=&ibDate=&ibTime="
+	xml= Nokogiri::HTML(open(url).read)
+
+	#get prices
+	price_string = xml.css("span.amount").to_s
+	price_string.gsub!(/\s+/, "")
+	price_string.gsub!(/[<\/spancl=""moutright>]/, "") #I'm sure there's a better way but...
+	prices = price_string.split('£')
+	prices.delete_at(0)
 	
-	#make a request to tripsta like: plane("INV", "LON", "211113")
-	def plane(origin, destination, date)
-		param_date = date[0,2] + "%2f" + date[2,2] + "%2f20" + date[4,2]
-		xml= Nokogiri::HTML(open("http://www.tripsta.co.uk/airline-tickets/results?dep=#{origin}&arr=#{destination}&passengersAdult=1&passengersChild=0&passengersInfant=0&class=&airlineCode=&directFlightsOnly=0&extendedDates=0&isRoundtrip=0&obDate=#{param_date}&obTime=&ibDate=&ibTime=").read)
-	
-		#get prices
-		price_string = xml.css("span.amount").to_s
-		price_string.gsub!(/\s+/, "")
-		price_string.gsub!(/[<\/spancl=""moutright>]/, "") #I'm sure there's a better way but...
-		prices = price_string.split('£')
-		prices.delete_at(0)
-		
-		#get durations
-		duration_string = xml.css('td.duration>span:first-child').inner_html
-		duration_string.gsub!(/\s+/, "")
-		duration_string = duration_string.scan(/\d*h\d*min/)
-		durations = Array.new
-		duration_string.each do |d|
-			dur = d.gsub('h', ':')
-			dur = dur.gsub!('min', '')
-			hours = dur[0, dur.index(':')]
-			mins = dur[dur.index(':')+1, dur.length]
-			if mins[0,1] == "0"
-				mins = mins[1,1]
-			end
-			durations.push(Integer(hours)*60 + Integer(mins))
+	#get durations
+	duration_string = xml.css('td.duration>span:first-child').inner_html
+	duration_string.gsub!(/\s+/, "")
+	duration_string = duration_string.scan(/\d*h\d*min/)
+	durations = Array.new
+	duration_string.each do |d|
+		dur = d.gsub('h', ':')
+		dur = dur.gsub!('min', '')
+		hours = dur[0, dur.index(':')]
+		mins = dur[dur.index(':')+1, dur.length]
+		if mins[0,1] == "0"
+			mins = mins[1,1]
 		end
-	
-		#prepare results
-		results  = Array.new
-		prices.count().times do |count|
-		  	pair = [prices[count], durations[count]]
-			results.push(pair)
-		end
-		return results
+		durations.push(Integer(hours)*60 + Integer(mins))
 	end
 
-	def best_journey(origin_id, destination_id, date)
-		#code here to pick the best options and return the three best results
-
-		#can use other methods in the meantime e.g. from views and contollers
-		#puts train("INV", "ABD", "211113").to_s
-		#puts bus("49", "56", "211113").to_s
-		#puts plane("INV", "LON", "211113").to_s
+	#prepare results
+	results  = Array.new
+	prices.count().times do |count|
+	  	pair = [prices[count], durations[count]]
+		results.push(pair)
 	end
+	return results, url
+end
+
+def best_journey(origin, destination, date)
+	#train_results = train(origin.station, destination.station, date)
+	#bus_results = bus(origin.bus, destination.bus, date)
+	#plane_results = plane(origin.airport, destination.airport, date)
+	#return [train_results[0], best_result(train_results[0]), train_results[1]], [bus_results[0], best_result(bus_results[0]), bus_results[1]],[plane_results[0], best_result(plane_results[0]), plane_results[1]]
+	
+	return [[[["77.50", 456], ["34.00", 420], ["84.50", 459], ["77.50", 464], ["84.50", 463]], [0.702, 1], "http://ojp.nationalrail.co.uk/service/timesandfares/ABD/London/111213/0900/dep"], [[["13.00", 770], ["13.00", 775], ["13.00", 745], ["13.00", 690]], [0.963, 3], "https://uk.megabus.com/JourneyResults.aspx?originCode=1&destinationCode=56&outboundDepartureDate=11%2f12%2f2013&passengerCount=1&transportType=-1"], [[["45.99", 100], ["47.99", 100], ["55.62", 100], ["69.99", 100], ["73.06", 100], ["75.60", 100], ["88.30", 100], ["100.16", 95], ["112.01", 90], ["155.83", 90]], [0.792, 0], "http://www.tripsta.co.uk/airline-tickets/results?dep=ABZ&arr=LON&passengersAdult=1&passengersChild=0&passengersInfant=0&class=&airlineCode=&directFlightsOnly=0&extendedDates=0&isRoundtrip=0&obDate=11%2f12%2f2013&obTime=&ibDate=&ibTime="]]
+end
+
+def best_result(results)
+	prices = Array.new
+	durations = Array.new
+	results.each do |r|
+		prices.push(r[0].to_f)
+		durations.push(r[1])
+	end
+	
+	#variables for test
+	prices_mean = DescriptiveStatistics::Stats.new(prices).mean
+	durations_mean = DescriptiveStatistics::Stats.new(durations).mean
+	
+	#adjust to scores
+	prices.count().times do |count|
+		prices[count] = (prices[count]/prices_mean)
+	end
+	durations.count().times do |count|
+		durations[count] = (durations[count]/durations_mean)
+	end
+	
+	#calc avg scores
+	scores = Array.new
+	prices.count().times do |count|
+		scores.push(DescriptiveStatistics::Stats.new([prices[count],durations[count]]).mean.round(3))
+	end
+	return scores.each_with_index.min
+end
+  
 end
