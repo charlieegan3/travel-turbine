c839b790d3c0f4b5410defdafd267829f996af5f
Charlie <charlieegan3@gmail.com>
2013-11-04 11:49:11 +0000

    refactored jouney disp. general tidy

--- app/controllers/journeys_controller.rb
+++ app/controllers/journeys_controller.rb
@@ -14,30 +14,10 @@ class JourneysController < ApplicationController
 		@journey.train_result = @data[1][0].to_s
 		@journey.bus_result = @data[1][1].to_s
 		@journey.plane_result = @data[1][2].to_s
-		@journey.save
-#		@origin = Location.all.pluck("name")  # Using @origin for both origin and destination fields. No need to duplicate data.
-#		@date = Date.new
+		#@journey.save #to stop creating uneeded elements in testing
+
+		#things for the view
+		@train_result = @journey.train_result.split(',')
 	end
-
-
-	def create
-		# Take the stuff selected in the form, and create a new 'journey' with it.
-
-		@journey.origin = params[:origin]
-		@journey.destination = params[:destination]
-		@journeydate = params[:date]   # Journey table doesn't have date field. (See Schema.rb)
-		@journey.train_result = train() # Need to include the location codes.
-
-		# USE THE METHODS IN STATIC_CONTROLLER ?
-		# Idea:    Make @origin in #new a 2-D array:   [ [CODE, NAME], [CODE, NAME], [CODE, NAME] ] etc...
-		# OR:  Create a 'Location' model that contains an id field, and a name attribute.
-
-		if @journey.save
-			redirect_to :root
-			# or display journey...
-		end
-		# else, throw error
-	end
-
 
 end
--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,17 +1,6 @@
 class StaticController < ApplicationController
-require 'open-uri'
-
+	require 'open-uri'
 	def index
 		# Currently just displays static/index.html.erb
 	end
-	# Other actions could go here.
-
-	def search
-		orig = Location.find_by_name(params[:origin]) #all loc's have uniq names
-		dest = Location.find_by_name(params[:destination])
-		date = params[:date]
-
-		@data = best_journey(orig,dest,date)
-	end
-
 end
