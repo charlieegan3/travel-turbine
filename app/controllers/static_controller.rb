bf3fa0721262e5ecb992142e67d4d27622f836f7
Charlie <charlieegan3@gmail.com>
2013-11-06 11:43:27 +0000

    added random project names

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,9 @@
 class StaticController < ApplicationController
 	require 'open-uri'
 	def index
+		nouns = ["Travel", "Trip", "Trek", "Expedition", "Excursion", "Outing", "Foray", "Cruise", "Vacation", "Holiday", "Adventure", "Passage", "Quest", "Epic Journey", "Journey"]
+		actions = ["Check", "Engine", "Turbine", "Checker", "Explorer", "Inquirer", "Detective", "Investigator"]
+		@project_name = nouns.sample + " " + actions.sample
 		@locations = Location.all.order("name ASC")
 	end
 end
