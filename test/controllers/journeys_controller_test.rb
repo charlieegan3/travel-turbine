06c2c1d6b3137490adcb60c740a0092727224d76
Dylan Hanning <dylanhanning123@hotmail.com>
2013-11-10 15:13:21 +0000

    Fixed wrong column field for journeys fixture

--- test/controllers/journeys_controller_test.rb
+++ test/controllers/journeys_controller_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class JourneysControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "TEST NOT NIL" do
+   	puts "i am a test"
+     @journey = Journey.all
+     assert @journey.nil? == false
+   end
+
+   test "journeys with aberdeen" do
+   	puts "There are journeys with origin = aberdeen in the DB"
+   	assert Journey.find_by_origin("Aberdeen").nil? == false
+   	end
+
 end
