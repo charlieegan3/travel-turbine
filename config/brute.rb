39dc0e13248e0b33bfa4db6efbe914759f7865e7
Dylan Hanning <dylanhanning123@hotmail.com>
2013-11-12 23:23:08 +0000

    Fixes and refactors for brute.rb

--- config/brute.rb
+++ config/brute.rb
@@ -16,17 +16,20 @@ require 'open-uri'
 #make a request to national rail like: train("INV", "ABD", "211113")
 def test
 	locations = ["Aberdeen", "Birmingham", "Bournemouth", "Bristol", "Cambridge", "Cardiff", "Carlisle", "Coventry", "Dundee", "Edinburgh", "Exeter", "Glasgow", "Inverness", "Leeds", "Liverpool", "London", "Manchester", "Newcastle", "Newquay", "Norwich", "Oban", "Penzance", "Perth", "Southampton", "Swansea", "Blackpool", "Chester"]
-	months = *(1..12)
+	orig = locations.sample	
+	locations = locations - [orig]
+	dest = locations.sample
+	months = *(11..12)
 	month = months.sample
-	case month
+	case month 				#  NOTE:  currently have to set months and days ranges manually (try using Time.now date to do it dynamically)
 		when 31
-			days = *(1..31)    # Must be a way to do this on one line...  *(0..31).sample doesn't work.
+			days = *(13..31)    # Must be a way to do this on one line...  *(0..31).sample doesn't work.
 			day = days.sample
 		when 30
-			days = *(1..30)
+			days = *(13..30)
 			day = days.sample
 		else
-			days = *(1..28)
+			days = *(13..28)
 			day = days.sample
 	end
 	date = "#{day}#{month}13"
@@ -34,23 +37,31 @@ def test
 	l1 = locations.sample
 	l2 = locations.sample
 	time = Time.now
-	xml = Nokogiri::HTML(open("http://travel-turbine.herokuapp.com/journey/new?origin=#{l1}&destination=#{l2}&date=#{date}").read)
+	begin
+		xml = Nokogiri::HTML(open("http://travel-turbine.herokuapp.com/journey/new?origin=#{l1}&destination=#{l2}&date=#{date}").read)
+	rescue # if there is an error, don't stop whole program.
+		puts "ERROR WITH REQUEST: new?origin=#{l1}&destination=#{l2}&date=#{date}"
+		return nil
+	end
 	time_dif = Time.now - time
-	puts "thread1: " + l1.to_s + " to " + l2.to_s + " on" + date + " - " + time_dif.to_s
+	puts "thread1: " + l1.to_s + " to " + l2.to_s + " on " + date + " - " + time_dif.to_s
 end
 def test2
 	locations2 = ["Aberdeen", "Birmingham", "Bournemouth", "Bristol", "Cambridge", "Cardiff", "Carlisle", "Coventry", "Dundee", "Edinburgh", "Exeter", "Glasgow", "Inverness", "Leeds", "Liverpool", "London", "Manchester", "Newcastle", "Newquay", "Norwich", "Oban", "Penzance", "Perth", "Southampton", "Swansea", "Blackpool", "Chester"]
-	months2 = *(1..12)
+	orig2 = locations2.sample	
+	locations2 = locations2 - [orig]
+	dest2 = locations2.sample
+	months2 = *(11..12)
 	month2 = months2.sample
-	case month2
+	case month2 				#  NOTE:  currently have to set months and days ranges manually (try using Time.now date to do it dynamically)
 		when 31
-			days2 = *(1..31)    # Must be a way to do this on one line...  *(0..31).sample doesn't work.
+			days2 = *(13..31)    # Must be a way to do this on one line...  *(0..31).sample doesn't work.
 			day2 = days2.sample
 		when 30
-			days2 = *(1..30)
+			days2 = *(13..30)
 			day2 = days2.sample
 		else
-			days2 = *(1..28)
+			days2 = *(13..28)
 			day2 = days2.sample
 	end
 	date2 = "#{day2}#{month2}13"
@@ -60,15 +71,28 @@ def test2
 	time2 = Time.now
 	xml2 = Nokogiri::HTML(open("http://travel-turbine.herokuapp.com/journey/new?origin=#{l12}&destination=#{l22}&date=#{date2}").read)
 	time_dif2 = Time.now - time2
-	puts "thread2: " +  l12.to_s + " to " + l22.to_s + " on" + date2 +" - " +time_dif2.to_s
+	puts "thread2: " +  l12.to_s + " to " + l22.to_s + " on " + date2 +" - " +time_dif2.to_s
 end
 
-50.times do
-	puts "---------------------"
-	t1=Thread.new{test()}
-	t2=Thread.new{test2()}
-	t1.join
-	t2.join
-	sleep(5)  # Wait 5 seconds - Note, server still dies a lot. Might be b/c of the threads.
+
+def stress_test
+	50.times do
+		puts "---------------------"
+		t1=Thread.new{test()}
+		t2=Thread.new{test2()}
+		t1.join
+		t2.join
+		sleep(5)  # Wait 5 seconds - Note, server still dies a lot. Might be b/c of the threads.
+	end
 end
 
+def single_thread(no, rest)  # rest in seconds
+	puts ""
+	puts "-> Doing a single_thread test with #{no} requests"
+	no.times do |n|
+		puts "-------#{n+1}---------"
+		test()
+		sleep(rest)
+	end
+end
+single_thread(50, 5) # 50 requests, 5 second interval
